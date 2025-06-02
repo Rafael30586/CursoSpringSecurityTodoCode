2c6bab78db75a6ba2f64ee26cfdd41d61ea61c0d
Rafael305 <fraq86@gmail.com>
2025-06-02 11:48:20 -0300

    preauthorize y enablemthodsecurity

--- spring-security2/src/main/java/com/todocodeacademy/spring/security2/controller/HelloWorldController.java
+++ spring-security2/src/main/java/com/todocodeacademy/spring/security2/controller/HelloWorldController.java
@@ -1,18 +1,27 @@
 package com.todocodeacademy.spring.security2.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize("denyAll()")
 public class HelloWorldController {
 
     @GetMapping("/holaseg")
+    @PreAuthorize("hasAuthority('READ')")
     public String secHelloWorld(){
         return "Hola mundo todocode con seguridad";
     }
 
     @GetMapping("/holanoseg")
+    @PreAuthorize("permitAll()")
     public String noSecHelloWorld(){
         return "Hola mundo todocode sin seguridad";
+    }
+
+    @GetMapping("/holanopreauth")
+    public String NoPreAuthHelloWorld(){
+        return "Hola mundo sin PreAuthorize";
     }
 }
